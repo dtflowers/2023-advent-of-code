1f34d6afdfc1e4f1ffae5aa5d8a74d4769d5fdf9
Doug Flowers <19670789+dtflowers@users.noreply.github.com>
2023-12-02 00:13:32 -0600

    add part 2 solution

--- challenges/day_2/solution.rb
+++ challenges/day_2/solution.rb
@@ -41,3 +41,30 @@ puts "The solution to part one of the challenge is #{sum}."
 ### END OF PART 1 SOLUTION ####
 
 ### BEGINNING OF PART 2 SOLUTION ####
+sum = input.each.map do |game|
+  minimum_blues = 0
+  minimum_greens = 0
+  minimum_reds = 0
+
+  results = game.split(": ").last.split("; ")
+
+  results.each do |result|
+    cubes = result.strip.split(", ")
+    cubes.each do |cube|
+      quantity = cube.split.first.to_i
+      color = cube.split.last
+      case color
+      when "blue"
+        minimum_blues = quantity if quantity > minimum_blues
+      when "green"
+        minimum_greens = quantity if quantity > minimum_greens
+      when "red"
+        minimum_reds = quantity if quantity > minimum_reds
+      end
+    end
+  end
+  minimum_blues * minimum_greens * minimum_reds
+end.sum
+
+puts "The solution to part two of the challenge is #{sum}."
+### END OF PART 2 SOLUTION ####
