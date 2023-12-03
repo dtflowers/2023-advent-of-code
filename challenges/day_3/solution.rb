ad9e1c5f0614a9757c6986b7eecc7e819da82b98
Doug Flowers <19670789+dtflowers@users.noreply.github.com>
2023-12-03 12:43:50 -0600

    stash progress, wip

--- challenges/day_3/solution.rb
+++ challenges/day_3/solution.rb
@@ -1,6 +1,17 @@
 ### BEGINNING OF PART 1 SOLUTION ####
 lines = File.readlines("test.txt")
 
+# specials = []
+# lines.each do |line|
+#   characters = line.tr("a-zA-Z", "").tr("0-9", "").tr(".", "").strip.chars.uniq
+#   characters.each do |character|
+#     specials << character
+#   end
+# end
+
+# specials = specials.uniq
+# pp specials
+
 numbers_coordinates = []
 symbol_coordinates = []
 
@@ -9,25 +20,28 @@ lines.each_with_index do |line, outer_index|
   items.reject!(&:empty?) if items.count > 1
   items.each do |item|
     length = item.length
-    is_number = item.scan(/[!@#$%^&*()_+{}\[\]:;'"\/\\?><.,]/).empty?
+    is_number = item.scan(/\W/).empty?
     if is_number
-      number = item
       numbers_coordinates << {
-        key: number,
-        value: number.to_i,
-        starting_coordinates: "(#{line.index(number)}, #{outer_index})"
+        key: item,
+        value: item.to_i,
+        starting_x_coordinate: line.index(item),
+        y_coordinate: outer_index
       }
     elsif length == 1
+      # NOTE WE ARE SO CLOSE BUT WE HAVE TO HANDLE THESE MULTIPLE CHARACTERS!!
       symbol = item
       symbol_coordinates << "(#{line.index(symbol)}, #{outer_index})"
     else
       numbers = item.scan(/\d+/)
-      symbols = item.scan(/[!@#$%^&*()_+{}\[\]:;'"\/\\?><.,]/)
-      numbers.each_with_index do |number, inner_index|
+      symbols = item.scan(/\W/)
+      pp symbols
+      numbers.each do |number|
         numbers_coordinates << {
           key: number,
           value: number.to_i,
-          starting_coordinates: "(#{line.index(number)}, #{outer_index})"
+          starting_x_coordinate: line.index(number),
+          y_coordinate: outer_index
         }
       end
       symbols.each_with_index do |symbol, inner_index|
@@ -37,7 +51,38 @@ lines.each_with_index do |line, outer_index|
   end
 end
 
-pp numbers_coordinates
+positions = numbers_coordinates.each.map do |number_coordinates|
+  adjacent_positions = []
+  number_coordinates[:key].chars.each_with_index do |_digit, index|
+    position = "(#{number_coordinates[:starting_x_coordinate] + index}, #{number_coordinates[:y_coordinate]})"
+    coordinates = position.scan(/\d+/)
+    x = coordinates.first.to_i
+    y = coordinates.last.to_i
+    adjacent_positions << position
+    adjacent_positions << "(#{x - 1}, #{y})"
+    adjacent_positions << "(#{x - 1}, #{y - 1})"
+    adjacent_positions << "(#{x}, #{y - 1})"
+    adjacent_positions << "(#{x}, #{y + 1})"
+    adjacent_positions << "(#{x - 1}, #{y + 1})"
+    adjacent_positions << "(#{x + 1}, #{y})"
+    adjacent_positions << "(#{x + 1}, #{y - 1})"
+    adjacent_positions << "(#{x + 1}, #{y + 1})"
+  end
+  { value: number_coordinates[:value], positions: adjacent_positions.uniq }
+end
+
+# pp positions
+
+part_numbers = []
+
+positions.each do |position|
+  unless (position[:positions].intersection symbol_coordinates).empty?
+    part_numbers << position[:value]
+  end
+end
+
+# pp positions
 
 pp symbol_coordinates
 
+pp part_numbers.sum
