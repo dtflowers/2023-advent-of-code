aae9b8c8c76314fa605c5b34e309b99b4dd9427a
Doug Flowers <19670789+dtflowers@users.noreply.github.com>
2023-12-08 10:35:53 -0600

    add part two solutions

--- challenges/day_8/solution.rb
+++ challenges/day_8/solution.rb
@@ -1,4 +1,3 @@
-
 ### BEGINNING OF PART 1 SOLUTION ####
 input = File.read("input.txt").split("\n").reject { |string| string.empty? }
 
@@ -11,16 +10,48 @@ nodes = input.to_h do |line|
   ]
 end
 
-previous_node = "AAA"
 steps = 0
-
-until previous_node == "ZZZ"
+node = "AAA"
+until node == "ZZZ"
   pattern.each do |move|
     steps += 1
     index = move == "R" ? 1 : 0
-    previous_node = nodes[previous_node][index]
+    node = nodes[node][index]
   end
 end
 
 puts "The answer to part one is #{steps} total steps"
 ### END OF PART 1 SOLUTION ####
+
+### BEGINNING OF PART 2 SOLUTION ####
+previous_nodes = nodes.keys.select { |e| e.chars.last == "A" }
+steps = previous_nodes.map! do |previous_node|
+  node_steps = 0
+  until previous_node.chars.last == "Z"
+    pattern.each do |move|
+      node_steps += 1
+      index = move == "R" ? 1 : 0
+      previous_node = nodes[previous_node][index]
+    end
+  end
+  node_steps
+end.reduce(1, :lcm)
+
+puts "The answer to part two is #{steps} total steps"
+### END OF PART 2 SOLUTION ####
+
+# ### BEGIN ORIGINAL ATTEMPT AT PART 2 #####
+# steps = 0
+# previous_nodes = nodes.keys.select { |e| e.chars.last == "A" }
+# until previous_nodes.all? { |e| e.chars.last == "Z" }
+#   pattern.each do |move|
+#     steps += 1
+#     index = move == "R" ? 1 : 0
+#     previous_nodes.map! do |previous_node|
+#       nodes[previous_node][index]
+#     end
+#   end
+# end
+
+# puts "The answer to part two is #{steps} total steps"
+# ### END ORIGINAL ATTEMPT AT PART 2 #####
