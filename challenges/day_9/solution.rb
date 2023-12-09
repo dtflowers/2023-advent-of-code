09cd497a720d742554ce4ab18ead27eba0124018
Doug Flowers <19670789+dtflowers@users.noreply.github.com>
2023-12-09 16:56:59 -0600

    add solutions

--- challenges/day_9/solution.rb
+++ challenges/day_9/solution.rb
@@ -1,4 +1,4 @@
-### BEGINNING OF PART 1 SOLUTION ####
+### BEGINNING OF PART 1 & 2 SOLUTION ####
 history = File.readlines("input.txt").map do |line|
   line.strip.split.map(&:to_i)
 end
@@ -21,21 +21,23 @@ history.map! do |sequence|
 end
 
 next_values = history.map do |sequences|
-  i = 0
   sequences = sequences.reverse
-  sequences.map! do |sequence|
+  new_sequences = sequences.each_with_index.map do |sequence, i|
     if sequence.uniq.count == 1
       sequence << sequence.last
+      sequence.unshift(sequence.first)
     else
       sequence << (sequence.last + sequences[i - 1].last)
+      sequence.unshift((sequence.first - sequences[i - 1].first))
     end
-    i += 1
     sequence
   end
-  sequences.last.last
+  new_sequences.last
 end
 
-solution = next_values.sum
+first_solution = next_values.map(&:last).sum
+second_solution = next_values.map(&:first).sum
 
-puts "The answer to part one is #{solution}"
-### END OF PART 1 SOLUTION ####
+puts "The answer to part one is #{first_solution}"
+puts "The answer to part two is #{second_solution}"
+### END OF PART 1 & 2 SOLUTION ####
